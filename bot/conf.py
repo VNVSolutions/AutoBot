9ba3fe52c73999041bbb1b17bcfc5aef157d765d
Luzhnyi, Liubomyr <l.luzhnuy@gmail.com>
2024-05-28 16:37:51 +0300

    change docker and conf

--- bot/conf.py
+++ bot/conf.py
@@ -1,14 +1,13 @@
 from time import sleep
 
 import telebot
-from django.conf import settings
 
 TOKEN = '6452560014:AAEW22uvw4bMEU5FWVUBvRgbfnHbqdnyxzE'
 
-# Ініціалізуємо телеграм-бота
 bot = telebot.TeleBot(TOKEN)
 
 
-# Встановлюємо вебхук
-bot.remove_webhook()
-bot.set_webhook(url=settings.WEBHOOK_URL)
+def set_webhook():
+    from django.conf import settings
+    bot.remove_webhook()
+    bot.set_webhook(url=settings.WEBHOOK_URL)
